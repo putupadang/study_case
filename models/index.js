f5c64ef57977bf5f884c07abc3f0838dbc09c0e8
putupadang <putupadang13@gmail.com>
2023-03-05 03:17:58 +0800

    update model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,18 @@
 import Comments from "./comment";
 import Users from "./user";
 
+//user has many comments
+Users.hasMany(Comments, {
+  sourceKey: "id",
+  foreignKey: "userId",
+  as: "comments",
+});
+
+//comment belongs to user
+Comments.belongsTo(Users, {
+  sourceKey: "id",
+  foreignKey: "userId",
+  as: "user",
+});
+
 export { Comments, Users };
